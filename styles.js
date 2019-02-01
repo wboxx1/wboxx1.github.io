3d48b77f098d96ded7e5ed11bf486363d0b7cbcb
Will Boxx <wboxx1@gmail.com>
2019-01-31 18:26:59 -0600

    content update

--- styles.js
+++ styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\wboxx\Documents\Projects\Web\Will Boxx\will-boxx-dot-com\src\styles.scss */"./src/styles.scss");
+module.exports = __webpack_require__(/*! C:\Users\wboxx\Documents\Projects\Javascript\Web\Will Boxx\will-boxx-dot-com\src\styles.scss */"./src/styles.scss");
 
 
 /***/ })
